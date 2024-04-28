f53e9819fa2e4c1942b361897d26bce767d9e2b6
Bijan k <80640045+Bijan-K@users.noreply.github.com>
2024-04-28 16:21:55 +0330

    Setting routes

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Vocabulary Practice</title>
+</svelte:head>
---
+++ src/routes/stats/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Stats</title>
+</svelte:head>
